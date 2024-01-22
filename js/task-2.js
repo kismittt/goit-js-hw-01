6d439392893f79d0f58fc7dc8c3efb97cc63c974
bogdan karanda (kismit) <142267790+kismittt@users.noreply.github.com>
2024-01-22 11:29:40 +0200

    і

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,10 @@
-"Shipping to <country> will cost <totalPrice> credits" 
+function getShippingMessage(country, price, deliveryFee) {
+    var totalPrice = price + deliveryFee;
+    var message = "Shipping to " + country + " will cost " + totalPrice + " credits";
+    return message;
+}
+
+
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
