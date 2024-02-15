1ece5bc62f5b9e2f83fa16e6bf2ad791eb70bbbf
bogdan karanda (kismit) <142267790+kismittt@users.noreply.github.com>
2024-02-15 13:28:05 +0200

    sasdів

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,3 @@
-function getElementWidth(content, padding, border) {
-  
-    var contentWidth = parseFloat(content);
-    var paddingWidth = parseFloat(padding);
-    var borderWidth = parseFloat(border);
-    var totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    return totalWidth;
-}
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
